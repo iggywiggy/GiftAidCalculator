c01b0d10fd8ef3dccebfe2a797afcaf57ba26d84
Lynnsay Piper <lynnsaymay.piper@outlook.com>
2017-02-05 11:56:19 +0000

    Added Test Stub for out of range donation argument

---
+++ GiftAidCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace GiftAidCalculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void CalculatorFigtAid_ParamDonationAmount_Zero_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Fail();
+        }
+    }
+}
